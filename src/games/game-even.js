dc544a448e171d037cf6dec8a7511a6f91630eb2
Ekaterina <militereva@mail.ru>
2023-04-04 09:59:16 +0300

    Add game-even/js

---
+++ src/games/game-even.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+
+const gameEven = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const maxNumRange = 100;
+    const number = Math.floor(Math.random() * maxNumRange);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (number % 2 === 0) {
+      if (answer === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+        return;
+      }
+    }
+    if (number % 2 !== 0) {
+      if (answer === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+        return;
+      }
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default gameEven;
